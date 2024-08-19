28935869dca1f280cf5cb5495afae08182e33370
Fredy-Cruz <fredyjcr0601@gmail.com>
2024-08-19 00:17:17 -0600

    Ejercicio 2 corregido

--- js/Ejercicio2.js
+++ js/Ejercicio2.js
@@ -3,16 +3,22 @@ de lo siguiente:
 • Examen =20%
 • tareas = 40%
 • asistencia = 10%
-• investigación = 30%*/
+• investigación = 30%
+Al final deberá mostrar los datos del alumno, nombre, carnet y nota final.*/
 
 //Generar numeros aleatorios para probar
 const random = () =>{
     return Math.floor(Math.random() * 10) + 1;
 }
 
+//Calcular Nota final
 const finalGrade = (exam, homework, asistence, investigation) =>{
     return ((exam * 0.2) + (homework * 0.4) + (asistence * 0.1) + (investigation * 0.3)).toFixed(2);
 }
+
+//Datos del alumno
+let name = 'Fredy';
+let carnet = "AJJNDS0001"
 
 let exam = random();
 let homework = random();
@@ -20,4 +26,4 @@ let asistence = random();
 let investigation = random();
 
 console.log(`Examen: ${exam}, Tareas: ${homework}, Asitencia: ${asistence}, Investigacion: ${investigation}`)
-console.log(`Su nota final es: ${finalGrade(exam, homework, asistence, investigation)}`);
+console.log(`Nombre: ${name}, Carnet: ${carnet}, Nota final es: ${finalGrade(exam, homework, asistence, investigation)}`);
