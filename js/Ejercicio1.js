5809d2be4cc1bb25c8edd064dc493b1d06b36b7a
Fredy-Cruz <fredyjcr0601@gmail.com>
2024-08-18 23:41:37 -0600

    Proyecto creado y ejercicio uno hecho

---
+++ js/Ejercicio1.js
@@ -0,0 +1,6 @@
+/*EJERCICIO 1: Crear una función que en base a la edad que ingreso el usuario devolver un
+mensaje si la persona es mayor de edad o no. Utilizar para la condición el operador ternario*/
+
+let age = prompt('Ingresa tu edad:')
+
+console.log(age >= 18 ? "Eres mayor de edad" : "Eres menor de edad"); 
